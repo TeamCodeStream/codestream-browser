0688608be8ed19de87414fc89744b7f3230af411
Peter Pezaris <pez@codestream.com>
2020-09-06 01:41:06 -0400

    adds support for issues

--- src/injectors/github-injector.ts
+++ src/injectors/github-injector.ts
@@ -5,288 +5,343 @@ import { ButtonInjector, InjectorBase, checkIsBtnUpToDate } from "./injector";
 import { renderCodeStreamUrl, openEditor, IDEs } from "../utils";
 
 namespace CodeStreamify {
-  export const NAV_BTN_ID = "codestream-btn-nav";
-  export const NAV_BTN_CLASS = "codestream-nav-btn";
-  export const NAV_BTN_CLASS_SELECTOR = "." + NAV_BTN_CLASS;
+	export const NAV_BTN_ID = "codestream-btn-nav";
+	export const NAV_BTN_CLASS = "codestream-nav-btn";
+	export const NAV_BTN_CLASS_SELECTOR = "." + NAV_BTN_CLASS;
 
-  export const CSS_REF_BTN_CONTAINER = "codestream-btn-container";
-  export const CSS_REF_NO_CONTAINER = "no-container";
+	export const CSS_REF_BTN_CONTAINER = "codestream-btn-container";
+	export const CSS_REF_NO_CONTAINER = "no-container";
 }
 
 /**
  * This implementation currently assumes that there is only ever one button per page
  */
 export class GitHubInjector extends InjectorBase {
-  protected configProvider: ConfigProvider;
-  constructor(configProvider: ConfigProvider) {
-    super(configProvider, [
-      new PullInjector(),
-      new IssueInjector(),
-      // new FileInjector(),
-      // new NavigationInjector(),
-      // new EmptyRepositoryInjector(),
-    ]);
-    this.configProvider = configProvider;
-  }
+	protected configProvider: ConfigProvider;
+	constructor(configProvider: ConfigProvider) {
+		super(configProvider, [
+			new PullRequestInjector(),
+			new IssueInjector(),
+			new PullRequestLinkInjector(),
+			// new FileInjector(),
+			// new NavigationInjector(),
+			// new EmptyRepositoryInjector(),
+		]);
+		this.configProvider = configProvider;
+	}
 
-  canHandleCurrentPage(): boolean {
-    // TODO Does this work for GitHub Enterprise, too?
-    const metaTags = document.getElementsByTagName("meta");
-    for (let i = 0; i < metaTags.length; i++) {
-      const metaTag = metaTags[i];
-      if (metaTag.name === "hostname" && metaTag.content.includes("github")) {
-        return true;
-      }
-    }
-    return false;
-  }
+	canHandleCurrentPage(): boolean {
+		// TODO Does this work for GitHub Enterprise, too?
+		const metaTags = document.getElementsByTagName("meta");
+		for (let i = 0; i < metaTags.length; i++) {
+			const metaTag = metaTags[i];
+			if (metaTag.name === "hostname" && metaTag.content.includes("github")) {
+				return true;
+			}
+		}
+		return false;
+	}
 
-  checkIsInjected(): boolean {
-    const button = document.getElementById(`${CodeStreamify.NAV_BTN_ID}`);
-    const currentUrl = renderCodeStreamUrl(this.config.codestreamURL);
-    return checkIsBtnUpToDate(button, currentUrl);
-  }
+	checkIsInjected(): boolean {
+		const button = document.getElementById(`${CodeStreamify.NAV_BTN_ID}`);
+		const currentUrl = renderCodeStreamUrl(this.config.codestreamURL);
+		return checkIsBtnUpToDate(button, currentUrl);
+	}
 
-  async inject(): Promise<void> {
-    // ghInjection triggers an event whenever only parts of the GitHub page have been reloaded
-    ghInjection(() => {
-      if (!this.checkIsInjected()) {
-        this.injectButtons();
-      }
-    });
-  }
+	async inject(): Promise<void> {
+		// ghInjection triggers an event whenever only parts of the GitHub page have been reloaded
+		ghInjection(() => {
+			if (!this.checkIsInjected()) {
+				this.injectButtons();
+			}
+		});
+	}
 
-  async update(): Promise<void> {
-    this.injectButtons();
-  }
+	async update(): Promise<void> {
+		this.injectButtons();
+	}
+}
+
+class PullRequestLinkInjector {
+	constructor() {}
+
+	isApplicableToCurrentPage(): boolean {
+		return true;
+	}
+
+	inject(ide: string, autoOpen: boolean, checkoutBranch: boolean) {
+		if (!autoOpen) return;
+		document.querySelectorAll("A").forEach((element) => {
+			// match something that looks like /pull/2723/whatever
+			// or /pull/2723 [end of string]
+			const a = element as HTMLAnchorElement;
+			let href = a.href;
+			if (href.match(/\/pull\/\d+(\/|$)/)) {
+				// local URL; therefore we must be on github.com
+				if (href.startsWith("/")) {
+					const metaTags = document.getElementsByTagName("meta");
+					for (let i = 0; i < metaTags.length; i++) {
+						const metaTag = metaTags[i];
+						if (metaTag.name === "hostname") {
+							href = metaTag.content + href;
+						}
+					}
+				}
+				a.onclick = (ev) =>
+					openEditor(ide, { url: href, checkoutBranch, providerId: "github*com" });
+			}
+		});
+	}
 }
 
 abstract class ButtonInjectorBase implements ButtonInjector {
-  constructor(
-    protected readonly parentSelector: string,
-    protected readonly btnClasses: string,
-    protected readonly float: boolean = true,
-    protected readonly asFirstChild: boolean = false
-  ) {}
+	constructor(
+		protected readonly parentSelector: string,
+		protected readonly btnClasses: string,
+		protected readonly float: boolean = true,
+		protected readonly asFirstChild: boolean = false
+	) {}
 
-  abstract isApplicableToCurrentPage(): boolean;
+	abstract isApplicableToCurrentPage(): boolean;
+	abstract renderButton(ide: string, autoOpen: boolean, checkoutBranch: boolean): HTMLElement;
 
-  inject(ide: string, autoOpen: boolean, checkoutBranch: boolean) {
-    const actionbar = select(this.parentSelector);
-    if (!actionbar) {
-      return;
-    }
+	inject(ide: string, autoOpen: boolean, checkoutBranch: boolean) {
+		const actionbar = select(this.parentSelector);
+		if (!actionbar) {
+			return;
+		}
 
-    const oldBtn = document.getElementById(CodeStreamify.NAV_BTN_ID);
-    if (oldBtn) {
-      return;
-    }
+		const oldBtn = document.getElementById(CodeStreamify.NAV_BTN_ID);
+		if (oldBtn) {
+			return;
+		}
 
-    const btn = this.renderButton(ide, autoOpen, checkoutBranch);
+		const btn = this.renderButton(ide, autoOpen, checkoutBranch);
 
-    const btnGroup = actionbar.getElementsByClassName("BtnGroup");
-    if (
-      btnGroup &&
-      btnGroup.length > 0 &&
-      btnGroup[0].classList.contains("float-right")
-    ) {
-      actionbar.insertBefore(btn, btnGroup[0]);
-    } else if (this.asFirstChild && actionbar) {
-      actionbar.insertBefore(btn, actionbar.firstChild);
-    } else {
-      actionbar.appendChild(btn);
-    }
-  }
+		const btnGroup = actionbar.getElementsByClassName("BtnGroup");
+		if (btnGroup && btnGroup.length > 0 && btnGroup[0].classList.contains("float-right")) {
+			actionbar.insertBefore(btn, btnGroup[0]);
+		} else if (this.asFirstChild && actionbar) {
+			actionbar.insertBefore(btn, actionbar.firstChild);
+		} else {
+			actionbar.appendChild(btn);
+		}
+	}
 
-  protected toggleDropdown() {
-    const dd = document.getElementById("codestream-ide-dropdown");
-    const modal = document.getElementById("codestream-modal");
-    if (dd && modal) {
-      if (dd.classList.contains("hidden")) {
-        modal.classList.add("active");
-        dd.classList.remove("hidden");
-      } else {
-        modal.classList.remove("active");
-        dd.classList.add("hidden");
-      }
-    }
-  }
+	protected toggleDropdown() {
+		const dd = document.getElementById("codestream-ide-dropdown");
+		const modal = document.getElementById("codestream-modal");
+		if (dd && modal) {
+			if (dd.classList.contains("hidden")) {
+				modal.classList.add("active");
+				dd.classList.remove("hidden");
+			} else {
+				modal.classList.remove("active");
+				dd.classList.add("hidden");
+			}
+		}
+	}
 
-  protected reRenderButton(
-    ide: string,
-    autoOpen: boolean,
-    checkoutBranch: boolean
-  ) {
-    const oldBtn = document.getElementById(CodeStreamify.CSS_REF_BTN_CONTAINER);
-    if (oldBtn)
-      oldBtn.replaceWith(this.renderButton(ide, autoOpen, checkoutBranch));
-  }
+	protected reRenderButton(ide: string, autoOpen: boolean, checkoutBranch: boolean) {
+		const oldBtn = document.getElementById(CodeStreamify.CSS_REF_BTN_CONTAINER);
+		if (oldBtn) oldBtn.replaceWith(this.renderButton(ide, autoOpen, checkoutBranch));
+	}
 
-  protected renderButton(
-    ide: string,
-    autoOpen: boolean,
-    checkoutBranch: boolean
-  ): HTMLElement {
-    let classes = this.btnClasses + ` ${CodeStreamify.NAV_BTN_CLASS}`;
-    if (this.float) {
-      classes = classes + ` float-right`;
-    }
-
-    const container = document.createElement("div");
-    container.id = CodeStreamify.CSS_REF_BTN_CONTAINER;
-    container.className = classes;
-
-    const m = document.createElement("div");
-    m.id = "codestream-modal";
-    m.onclick = (ev) => {
-      this.toggleDropdown();
-    };
-
-    const selectedIde = IDEs.find((_) => _.moniker === ide) || IDEs[0];
-
-    if (autoOpen) {
-      openEditor(selectedIde.moniker, {
-        url: window.location.href,
-        checkoutBranch,
-      });
-    }
-
-    const a = document.createElement("a");
-    a.id = CodeStreamify.NAV_BTN_ID;
-    a.title = "CodeStream";
-    a.innerHTML = `<img width='14' height='14' style='vertical-align: -2px' src='https://images.codestream.com/ides/128/${selectedIde.moniker}.png'> Open in ${selectedIde.ideName}`;
-    // a.href = url;
-    a.onclick = (ev) => {
-      openEditor(selectedIde.moniker, {
-        url: window.location.href,
-        checkoutBranch,
-      });
-    };
-    a.target = "_blank";
-    // if (autoOpen) {
-    // makeOpenInPopup(a);
-    // }
-    a.className = "btn btn-sm";
-
-    const a2 = document.createElement("a");
-    a2.id = CodeStreamify.NAV_BTN_ID + "-2";
-    a2.title = "Open Options";
-    a2.innerHTML = "<span class='dropdown-caret'></span>";
-    a2.onclick = (ev) => {
-      this.toggleDropdown();
-    };
-    // a.href = url;
-    // a.target = "_blank";
-    // if (autoOpen) {
-    // makeOpenInPopup(a2);
-    // }
-    a2.className = "btn btn-sm";
-
-    this.adjustButton(a);
-
-    container.appendChild(m);
-    container.appendChild(a);
-    container.appendChild(a2);
-
-    const dropdown = document.createElement("div");
-    dropdown.id = "codestream-ide-dropdown";
-    dropdown.className =
-      "mt-1 position-absolute Box box-shadow-medium text-gray-dark right-0 dropdown hidden";
-
-    const ddBody = document.createElement("div");
-    ddBody.className = "select-menu-list";
-    dropdown.appendChild(ddBody);
-
-    const checkRow = document.createElement("div");
-    checkRow.className = "select-menu-item";
-    checkRow.title =
-      "Automatically open PRs in your IDE when you visit the PR web page.";
-    const check = document.createElement("input");
-    check.type = "checkbox";
-    check.checked = autoOpen;
-    checkRow.onclick = async (ev) => {
-      const configProvider = await ConfigProvider.create();
-      configProvider.setConfig({
-        autoOpen: !autoOpen,
-      });
-      this.reRenderButton(ide, !autoOpen, checkoutBranch);
-    };
-    checkRow.appendChild(check);
-    const checkLabel = document.createElement("span");
-    checkLabel.textContent = "Auto-open";
-    checkRow.appendChild(checkLabel);
-
-    const checkRow2 = document.createElement("div");
-    checkRow2.className = "select-menu-item";
-    checkRow2.title =
-      "Automatically check out to the branch once loaded in your IDE.";
-    const check2 = document.createElement("input");
-    check2.type = "checkbox";
-    check2.checked = checkoutBranch;
-    checkRow2.onclick = async (ev) => {
-      const configProvider = await ConfigProvider.create();
-      configProvider.setConfig({
-        checkoutBranch: !checkoutBranch,
-      });
-      this.reRenderButton(ide, autoOpen, !checkoutBranch);
-    };
-    checkRow2.appendChild(check2);
-    const checkLabel2 = document.createElement("span");
-    checkLabel2.textContent = "Checkout Branch";
-    checkRow2.appendChild(checkLabel2);
-
-    ddBody.appendChild(checkRow);
-    ddBody.appendChild(checkRow2);
-    ddBody.appendChild(document.createElement("HR"));
-
-    IDEs.forEach((ide) => {
-      const ideRow = document.createElement("div");
-      ideRow.innerHTML = `<img width='20' height='20' style='vertical-align: -4px; margin-right: 2px;' src='https://images.codestream.com/ides/128/${ide.moniker}.png'> ${ide.ideName}`;
-      ideRow.className = "select-menu-item";
-      if (ide.moniker.indexOf("jb-") === 0) {
-        // ideRow.title = "Requires the JetBrains Toolbox App";
-      }
-      ideRow.onclick = async (ev) => {
-        const configProvider = await ConfigProvider.create();
-        configProvider.setConfig({
-          ide: ide.moniker,
-        });
-        openEditor(ide.moniker, { url: window.location.href, checkoutBranch });
-        this.toggleDropdown();
-        this.reRenderButton(ide.moniker, autoOpen, checkoutBranch);
-      };
-      ddBody.appendChild(ideRow);
-      if (ide.sepAfter) {
-        ddBody.appendChild(document.createElement("HR"));
-      }
-    });
-
-    container.appendChild(dropdown);
-    return container;
-  }
-  protected adjustButton(a: HTMLAnchorElement) {
-    // do nothing
-  }
+	protected adjustButton(a: HTMLAnchorElement) {
+		// do nothing
+	}
 }
 
-class PullInjector extends ButtonInjectorBase {
-  constructor() {
-    super(".gh-header-actions", "");
-  }
+class PullRequestInjector extends ButtonInjectorBase {
+	constructor() {
+		super(".gh-header-actions", "cs-dropdown");
+	}
 
-  isApplicableToCurrentPage(): boolean {
-    return window.location.pathname.includes("/pull/");
-  }
+	isApplicableToCurrentPage(): boolean {
+		return window.location.pathname.includes("/pull/");
+	}
+
+	renderButton(ide: string, autoOpen: boolean, checkoutBranch: boolean): HTMLElement {
+		let classes = this.btnClasses + ` ${CodeStreamify.NAV_BTN_CLASS}`;
+		if (this.float) {
+			classes = classes + ` float-right`;
+		}
+
+		const container = document.createElement("div");
+		container.id = CodeStreamify.CSS_REF_BTN_CONTAINER;
+		container.className = classes;
+
+		const m = document.createElement("div");
+		m.id = "codestream-modal";
+		m.onclick = (ev) => {
+			this.toggleDropdown();
+		};
+
+		const selectedIde = IDEs.find((_) => _.moniker === ide) || IDEs[0];
+
+		if (autoOpen) {
+			openEditor(selectedIde.moniker, {
+				url: window.location.href,
+				checkoutBranch,
+				providerId: "github*com",
+			});
+		}
+
+		const a = document.createElement("a");
+		a.id = CodeStreamify.NAV_BTN_ID;
+		a.title = "CodeStream";
+		a.innerHTML = `<img width='14' height='14' style='vertical-align: -2px' src='https://images.codestream.com/ides/128/${selectedIde.moniker}.png'> Open in ${selectedIde.ideName}`;
+		// a.href = url;
+		a.onclick = (ev) => {
+			openEditor(selectedIde.moniker, {
+				url: window.location.href,
+				checkoutBranch,
+				providerId: "github*com",
+			});
+		};
+		a.target = "_blank";
+		// if (autoOpen) {
+		// makeOpenInPopup(a);
+		// }
+		a.className = "btn btn-sm";
+
+		const a2 = document.createElement("a");
+		a2.id = CodeStreamify.NAV_BTN_ID + "-2";
+		a2.title = "Open Options";
+		a2.innerHTML = "<span class='dropdown-caret'></span>";
+		a2.onclick = (ev) => {
+			this.toggleDropdown();
+		};
+		// a.href = url;
+		// a.target = "_blank";
+		// if (autoOpen) {
+		// makeOpenInPopup(a2);
+		// }
+		a2.className = "btn btn-sm";
+
+		container.appendChild(m);
+		container.appendChild(a);
+		container.appendChild(a2);
+
+		const dropdown = document.createElement("div");
+		dropdown.id = "codestream-ide-dropdown";
+		dropdown.className =
+			"mt-1 position-absolute Box box-shadow-medium text-gray-dark right-0 dropdown hidden";
+
+		const ddBody = document.createElement("div");
+		ddBody.className = "select-menu-list";
+		dropdown.appendChild(ddBody);
+
+		const checkRow = document.createElement("div");
+		checkRow.className = "select-menu-item";
+		checkRow.title = "Automatically open PRs in your IDE when you visit the PR web page.";
+		const check = document.createElement("input");
+		check.type = "checkbox";
+		check.checked = autoOpen;
+		checkRow.onclick = async (ev) => {
+			const configProvider = await ConfigProvider.create();
+			configProvider.setConfig({
+				autoOpen: !autoOpen,
+			});
+			this.reRenderButton(ide, !autoOpen, checkoutBranch);
+		};
+		checkRow.appendChild(check);
+		const checkLabel = document.createElement("span");
+		checkLabel.textContent = "Auto-open PRs";
+		checkRow.appendChild(checkLabel);
+
+		const checkRow2 = document.createElement("div");
+		checkRow2.className = "select-menu-item";
+		checkRow2.title = "Automatically check out to the branch once loaded in your IDE.";
+		const check2 = document.createElement("input");
+		check2.type = "checkbox";
+		check2.checked = checkoutBranch;
+		checkRow2.onclick = async (ev) => {
+			const configProvider = await ConfigProvider.create();
+			configProvider.setConfig({
+				checkoutBranch: !checkoutBranch,
+			});
+			this.reRenderButton(ide, autoOpen, !checkoutBranch);
+		};
+		checkRow2.appendChild(check2);
+		const checkLabel2 = document.createElement("span");
+		checkLabel2.textContent = "Checkout to Branch&nbsp;&nbsp;";
+		checkRow2.appendChild(checkLabel2);
+
+		ddBody.appendChild(checkRow);
+		ddBody.appendChild(checkRow2);
+		ddBody.appendChild(document.createElement("HR"));
+
+		IDEs.forEach((ide) => {
+			const ideRow = document.createElement("div");
+			ideRow.innerHTML = `<img width='20' height='20' style='vertical-align: -4px; margin-right: 2px;' src='https://images.codestream.com/ides/128/${ide.moniker}.png'> ${ide.ideName}`;
+			ideRow.className = "select-menu-item";
+			if (ide.moniker.indexOf("jb-") === 0) {
+				// ideRow.title = "Requires the JetBrains Toolbox App";
+			}
+			ideRow.onclick = async (ev) => {
+				const configProvider = await ConfigProvider.create();
+				configProvider.setConfig({
+					ide: ide.moniker,
+				});
+				openEditor(ide.moniker, {
+					url: window.location.href,
+					checkoutBranch,
+					providerId: "github*com",
+				});
+				this.toggleDropdown();
+				this.reRenderButton(ide.moniker, autoOpen, checkoutBranch);
+			};
+			ddBody.appendChild(ideRow);
+			if (ide.sepAfter) {
+				ddBody.appendChild(document.createElement("HR"));
+			}
+		});
+
+		container.appendChild(dropdown);
+		return container;
+	}
 }
 
 class IssueInjector extends ButtonInjectorBase {
-  constructor() {
-    super(".gh-header-actions", "");
-  }
+	constructor() {
+		super(".gh-header-actions", "");
+	}
 
-  isApplicableToCurrentPage(): boolean {
-    return window.location.pathname.includes("/issues/");
-  }
+	isApplicableToCurrentPage(): boolean {
+		return window.location.pathname.includes("/issues/");
+	}
+
+	renderButton(ide: string, autoOpen: boolean, checkoutBranch: boolean): HTMLElement {
+		let classes = this.btnClasses + ` ${CodeStreamify.NAV_BTN_CLASS}`;
+		if (this.float) {
+			classes = classes + ` float-right`;
+		}
+
+		const container = document.createElement("div");
+		container.id = CodeStreamify.CSS_REF_BTN_CONTAINER;
+		container.className = classes;
+
+		const selectedIde = IDEs.find((_) => _.moniker === ide) || IDEs[0];
+
+		const a = document.createElement("a");
+		a.id = CodeStreamify.NAV_BTN_ID;
+		a.title = `Start work in ${selectedIde.ideName}+CodeStream`;
+		a.innerHTML = "Start Work";
+		a.onclick = (ev) => {
+			openEditor(selectedIde.moniker, {
+				url: window.location.href,
+				checkoutBranch,
+				controller: "startWork",
+				providerId: "github*com",
+			});
+		};
+		a.target = "_blank";
+		a.className = "btn btn-sm";
+
+		container.appendChild(a);
+		return container;
+	}
 }
 
 // class FileInjector extends ButtonInjectorBase {
--- src/options/options.ts
+++ src/options/options.ts
@@ -1,51 +1,49 @@
 import { ConfigProvider } from "../config";
 
-const codestreamUrlInput = document.getElementById(
-  "codestream-url-input"
-)! as HTMLInputElement;
+const codestreamUrlInput = document.getElementById("codestream-url-input")! as HTMLInputElement;
 const codestreamAutoOpenInput = document.getElementById(
-  "codestream-auto-open"
+	"codestream-auto-open"
 )! as HTMLInputElement;
 const codestreamCheckoutBranchInput = document.getElementById(
-  "codestream-checkout-branch"
+	"codestream-checkout-branch"
 )! as HTMLInputElement;
 const messageElement = document.getElementById("message")! as HTMLDivElement;
 
 const init = async () => {
-  const configProvider = await ConfigProvider.create();
+	const configProvider = await ConfigProvider.create();
 
-  // Initialize UI
-  const initialConfig = configProvider.getConfig();
-  codestreamUrlInput.value = initialConfig.codestreamURL;
-  codestreamAutoOpenInput.checked = initialConfig.autoOpen;
-  codestreamCheckoutBranchInput.checked = initialConfig.checkoutBranch;
+	// Initialize UI
+	const initialConfig = configProvider.getConfig();
+	codestreamUrlInput.value = initialConfig.codestreamURL;
+	codestreamAutoOpenInput.checked = initialConfig.autoOpen;
+	codestreamCheckoutBranchInput.checked = initialConfig.checkoutBranch;
 
-  let timeout: number | undefined = undefined;
+	let timeout: number | undefined = undefined;
 
-  // Save config before close
-  const save = () => {
-    // Update config (propagated internally)
-    configProvider.setConfig({
-      codestreamURL: codestreamUrlInput.value || undefined,
-      autoOpen: codestreamAutoOpenInput.checked,
-    });
-    if (timeout) {
-      window.clearTimeout(timeout);
-      timeout = undefined;
-    }
-    messageElement.innerText = "Saved.";
-    timeout = window.setTimeout(() => {
-      messageElement.innerText = "";
-      timeout = undefined;
-    }, 3000);
-  };
-  codestreamUrlInput.addEventListener("keyup", (event: KeyboardEvent) => {
-    if (event.isComposing || event.keyCode === 229) {
-      return;
-    }
-    save();
-  });
-  codestreamAutoOpenInput.addEventListener("change", save);
+	// Save config before close
+	const save = () => {
+		// Update config (propagated internally)
+		configProvider.setConfig({
+			codestreamURL: codestreamUrlInput.value || undefined,
+			autoOpen: codestreamAutoOpenInput.checked,
+		});
+		if (timeout) {
+			window.clearTimeout(timeout);
+			timeout = undefined;
+		}
+		messageElement.innerText = "Saved.";
+		timeout = window.setTimeout(() => {
+			messageElement.innerText = "";
+			timeout = undefined;
+		}, 3000);
+	};
+	codestreamUrlInput.addEventListener("keyup", (event: KeyboardEvent) => {
+		if (event.isComposing || event.keyCode === 229) {
+			return;
+		}
+		save();
+	});
+	codestreamAutoOpenInput.addEventListener("change", save);
 };
 
 init().catch((err) => console.error(err));
